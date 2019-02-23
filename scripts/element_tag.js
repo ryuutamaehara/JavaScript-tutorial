8c6f399d2b38a33153eaa25419266a92e9c9f99b
ryuuta maehara <ryuutamaehara@gmail.com>
2019-02-23 20:24:52 +0900

    get element sample

---
+++ scripts/element_tag.js
@@ -0,0 +1,8 @@
+//ページ内のアンカータグをすべて取得
+var list = document.getElementsByTagName('a');
+
+//リスト(HTMLCollectionオブジェクト)から順にアンカータグを取り出し、
+//そのhref属性をログに出力する。
+for(var i = 0,len = list.length; i < len; i++){
+    console.log(list.item(i).href);
+}
